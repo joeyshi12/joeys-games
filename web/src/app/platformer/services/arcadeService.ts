9688c0a282f9e8cff5aae31517f7b6f5e4c6721f
Joey Shi <joeyshi12@gmail.com>
2021-12-22 02:26:18 -0800

    added empty methods to arcade controller

--- web/src/app/platformer/services/arcadeService.ts
+++ web/src/app/platformer/services/arcadeService.ts
@@ -2,5 +2,21 @@
  * Data service class to update player through web socket
  */
 export class ArcadeService {
+  private serverIp: string = "127.0.0.1";
+  private serverPort: string = "3000";
+  private local: boolean = true;
+
   constructor() {}
+
+  public startConnection(): void {
+
+  }
+
+  public updatePlayer(): void {
+
+  }
+
+  public endConnection(): void {
+
+  }
 }
