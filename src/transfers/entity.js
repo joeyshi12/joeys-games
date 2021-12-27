dc4bb700ba3fb19cd3406a779f7de9f0bee76f2c
Joey Shi <joeyshi12@gmail.com>
2021-12-26 22:09:22 -0800

    removed js files

--- src/transfers/entity.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PlayerState = void 0;
-var PlayerState;
-(function (PlayerState) {
-    PlayerState["falling"] = "falling";
-    PlayerState["landed"] = "landed";
-})(PlayerState = exports.PlayerState || (exports.PlayerState = {}));
-//# sourceMappingURL=entity.js.map
--- src/util/commonFunctions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isBlank = void 0;
-function isBlank(obj) {
-    return obj === undefined || obj === null;
-}
-exports.isBlank = isBlank;
-//# sourceMappingURL=commonFunctions.js.map
