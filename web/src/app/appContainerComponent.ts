06e029cbce02080744dba22e7b229ca26c408a02
Joey <joeyshi12@gmail.com>
2022-01-09 14:52:10 -0800

    Added metadata tags

--- web/src/app/appContainerComponent.ts
+++ web/src/app/appContainerComponent.ts
@@ -5,6 +5,7 @@ import { RendererService } from "./platformer/services/rendererService";
 import { SoundPlayerService } from "./platformer/services/soundPlayerService";
 import { StageService } from "./platformer/services/stageService";
 import { Socket } from "ngx-socket-io";
+import {Meta} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -18,7 +19,13 @@ export class AppContainerComponent implements OnInit {
               playerDataService: PlayerDataService,
               rendererService: RendererService,
               soundPlayerService: SoundPlayerService,
-              stageService: StageService) {
+              stageService: StageService,
+              meta: Meta) {
+    meta.addTags([
+      {name: "author", content: "Joey Shi"},
+      {name: "description", property: "og:description", content: "Real-time multiplayer browser platformer made with p5"},
+      {name: "image", property: "og:image", content: "https://raw.githubusercontent.com/joeyshi12/platform-party/main/assets/snapshot.png"}
+    ]);
     this._sketch = new PlatformerSketch(socket, playerDataService, rendererService, soundPlayerService, stageService);
   }
 
