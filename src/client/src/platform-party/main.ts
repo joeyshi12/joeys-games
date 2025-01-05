c41da2978f5bd9e70cd18e076a3409f0493eb2ba
Joey Shi <joeyshi12@gmail.com>
2025-01-04 17:12:59 -0800

    enforce websocket protocol

--- src/client/src/platform-party/main.ts
+++ src/client/src/platform-party/main.ts
@@ -1,8 +1,9 @@
 import PlatformPartyManager from "./platformPartyManager";
 import { io } from "socket.io-client";
 
+// SERVICE_URL is a webpack environment variable
 // @ts-ignore
-const socket: Socket = io(SERVICE_URL); // webpack environment variable
+const socket: Socket = io(SERVICE_URL, { transports: ["websocket"] });
 
 const game = new PlatformPartyManager("#canvas-container", socket);
 game.start();
